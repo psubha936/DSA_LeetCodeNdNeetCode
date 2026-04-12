7e7b25fb691ec28d28ed72a67aa320c688ba03f3
Subhaprakash Nayak <89669175+psubha936@users.noreply.github.com>
2026-04-13 01:01:59 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit =0;
+        let minPrice = Infinity;
+        for(let price of prices){
+            if(price < minPrice){
+                minPrice = price
+            }else{
+                maxProfit = Math.max(maxProfit, price-minPrice)
+            }
+        }
+        return maxProfit
+    }
+}
